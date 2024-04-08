51ce8508aa2dc587a6f021196c63ea4b81433451
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-04-08 19:28:04 +1000

    icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 export default function Home() {
-  return (
-    <main
-      className={cn(
-        "bg-main-600 flex min-h-screen flex-col items-center justify-center p-24"
-      )}
-    >
-      <Image
-        className="-mb-16"
-        src="/hat.png"
-        alt=""
-        height="500"
-        width="500"
-      />
-      <div className="text-accent-500 text-6xl">Murasaki</div>
-    </main>
-  );
+	return (
+		<main className={cn('flex min-h-screen flex-col items-center justify-center bg-[#4267B2] p-24')}>
+			<div className="text-accent-500 text-6xl text-white">formerly facebook</div>
+		</main>
+	);
 }
